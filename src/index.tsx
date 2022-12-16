567f7f8b13aa151955eaf9a18c12c26dcf223c5f
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-12-16 09:24:06 -0400

    Added Material UI & react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import CssBaseline from '@mui/material/CssBaseline';
 import App from './App'
 
 const rootElement = document.getElementById('root')
@@ -11,5 +17,8 @@ if (!rootElement) {
 
 const root = createRoot(rootElement)
 root.render(
-  <App />
+  <>
+    <CssBaseline />
+    <App />
+  </>
 )
