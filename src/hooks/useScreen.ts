73c0b7130e9515920a504692e7ffee38cbc98634
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-12-27 16:44:35 -0400

    useScreen hook

---
+++ src/hooks/useScreen.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+
+type ScreenProps = {
+  width: number,
+  height: number,
+}
+
+const useScreen = (): ScreenProps => {
+  const [screen, setScreen] = useState<ScreenProps>({ width: window.innerWidth, height: innerHeight })
+
+  const screenListener = () => {
+    const { innerWidth: width, innerHeight: height } = window
+    setScreen({ width, height })
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', screenListener)
+    return () => {
+        window.removeEventListener('resize', screenListener)
+    }
+  }, [window.innerWidth, window.innerHeight])
+
+  return screen
+}
+
+export default useScreen
