2f987dac20e8a013e511b1d4256c7b7b7dd7fbb7
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-12-27 16:46:01 -0400

    Added useScreen hook to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import useScreen from './hooks/useScreen'
 
 import Box from '@mui/material/Box'
-import Header from './components/header'
+import Header from './components/Header'
 
 /* Pages components */
 import LoginPage from './pages/Login'
 
 const App = () => {
+  const { height } = useScreen()
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+    <Box sx={{ display: 'flex', flexDirection: 'column', height }}>
       <Header />
       <Box sx={{ flex: 1 }}>
         <LoginPage />
