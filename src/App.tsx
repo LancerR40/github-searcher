162c1bb4f551b7ae0a94a10826a76a78c1a38a2a
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-12-27 11:00:09 -0400

    Added sub-components to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import Box from '@mui/material/Box'
+import Header from './components/header'
+
+/* Pages components */
+import LoginPage from './pages/Login'
+
 const App = () => {
   return (
-    <div>Hello World</div>
+    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+      <Header />
+      <Box sx={{ flex: 1 }}>
+        <LoginPage />
+      </Box>
+    </Box>
   )
 }
 
