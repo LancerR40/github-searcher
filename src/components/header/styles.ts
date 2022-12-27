ba0a929afdba9f68e30bec4b8288e63fa7c526fc
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-12-27 10:59:14 -0400

    Header component

---
+++ src/components/header/styles.ts
@@ -0,0 +1,27 @@
+const styles = {
+  icon: {
+    display: {
+      md: 'none',
+    },
+  },
+  title: {
+    fontWeight: 700,
+    display: {
+      xs: 'none',
+      md: 'block',
+    }
+  },
+  linkButtonContainer: {
+    flexGrow: 0, 
+    display: 'flex', 
+    alignItems: 'center',
+  },
+  linkButton: {
+    display: 'block',
+    my: 2, 
+    mx: 1,
+    color: 'text.primary',
+  },
+}
+
+export default styles
