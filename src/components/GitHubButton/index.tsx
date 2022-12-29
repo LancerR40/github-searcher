41fc9e0c90e07bff3f5044967ebeb3915759b723
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-12-28 21:14:49 -0400

    Added GitHub login button component

--- src/components/GitHubButton/index.tsx
+++ src/components/GitHubButton/index.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 import Button from '@mui/material/Button'
 import GitHub from '@mui/icons-material/GitHub'
 
+const CLIENT_ID = 'd96841e286bcfb16867b'
+const REDIRECT_URI = 'http://localhost:3000/'
+
 const GitHubButton = () => {
+  const link = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`
   return (
     <Button
       sx={{ bgcolor: '#1976D2', color: '#fff' }}
-      variant="contained" 
+      variant="contained"
       startIcon={<GitHub />}
       size='large'
+      href={link}
       disableElevation
       fullWidth
     >
