27836a805fda737e6e81afa38a1301f742646a09
Ronald Abu Saleh <lancerrnld@gmail.com>
2023-01-05 19:11:51 -0400

    NotFound, Spinner + code typing

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState, memo, useMemo } from 'react'
+import React, { useEffect, useState } from 'react'
 import Searcher from '../../components/Searcher'
 import UserCard from '../../components/UserCard'
 import Container from '@mui/material/Container'
+import NotFound from '../../components/NotFound'
 import CircularProgress from '@mui/material/CircularProgress'
 import styles from './styles'
 import useTheme from '@mui/material/styles/useTheme'
@@ -9,6 +10,16 @@ import useMediaQuery from '@mui/material/useMediaQuery'
 import styled from '@mui/material/styles/styled'
 import API from '../../api'
 import { GitHubUser } from '../../types'
+
+type UserNotFound = {
+  isNotFound: boolean,
+  message: string,
+}
+
+const notFoundInitialState = {
+  isNotFound: false,  
+  message: '',
+}
 
 const CustomContainer = styled(Container)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'light' ? '#f2f2f2' : '#161e35'
@@ -21,7 +32,8 @@ const HomePage = () => {
 
   const [text, setText] = useState<string>('')
   const [username, setUsername] = useState<string>('Octocat')
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState<GitHubUser | {}>({})
+  const [notFound, setNotFound] = useState<UserNotFound>(notFoundInitialState)
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
   useEffect(() => {
@@ -29,12 +41,13 @@ const HomePage = () => {
   }, [username])
   
   const getGitHubUser = async () => {
+    setNotFound(notFoundInitialState)
     setIsLoading(true)
     const response = await API.getGitHubUsersByUsername(username)
     setIsLoading(false)
 
     if (response.result === 'error') {
-      return alert(response.message)
+      return setNotFound({ isNotFound: true, message: 'Ups! user not found 🧑🏻‍🚀 ...' })
     }
 
     setUser(response.data)
@@ -48,6 +61,10 @@ const HomePage = () => {
     setText(e.target.value)
   }
 
+  const renderUser = notFound.isNotFound 
+    ? <NotFound sx={{ mx: 'auto', mt: 3, }} message={notFound.message} /> 
+    : <UserCard user={user as GitHubUser} />
+
   return (
     <CustomContainer sx={styles.container} maxWidth={containerMaxWidth} disableGutters>
       <Searcher 
@@ -59,8 +76,8 @@ const HomePage = () => {
       />
 
       {isLoading === true 
-        ? <CircularProgress sx={{ mt: 4 }} /> 
-        : <UserCard user={user as GitHubUser} />
+        ? <CircularProgress sx={{ mt: 4, mx: 'auto', }} /> 
+        : renderUser
       }
     </CustomContainer>
   )
