5a93137d12d823bd8e0f6d3f701439fbb6990866
Ronald Abu Saleh <lancerrnld@gmail.com>
2023-01-05 19:05:19 -0400

    Login Page component has been removed

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Box from '@mui/material/Box'
 import Header from './components/Header'
 
 /* Pages components */
-import LoginPage from './pages/Login'
 import HomePage from './pages/Home'
 
 const App = () => {
@@ -14,7 +13,6 @@ const App = () => {
     <Box sx={{ display: 'flex', flexDirection: 'column', height }}>
       <Header />
       <Box sx={{ flex: 1 }}>
-        {/* <LoginPage /> */}
         <HomePage />
       </Box>
     </Box>
